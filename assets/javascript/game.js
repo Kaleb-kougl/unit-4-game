f03a64c64dd2ed130ae8143b96eac148def9d27e
Kaleb Kougl <kalebkougl@gmail.com>
2018-11-13 19:16:49 -0600

    adding text for defeating enemy, adding hp to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -62,15 +62,21 @@ function handleAttackBtnClick() {
   if (enemySet && enemyhp >= 0 && charhp >= 0) {
     charhp -= enemyAttack;
     enemyhp -= currentAttack;
-    $('#attack-output').html(`<div class="col-4">You attacked ${enemyName} for 
-    ${currentAttack} damage.<br>${enemyName} attacked you back for ${enemyAttack} damage.</div>`);
+    $('#attack-output').html(`<div class="col-4">You have defeated ${enemyName}. You can chooe another enemy to fight.</div>`);
+    $(`#${charName}`).text(charhp);
+    $(`#${enemyName}`).text(enemyhp);
     currentAttack += baseCharAttack;
-  } else if (enemyhp <= 0) {
+  } 
+  
+  if (enemyhp <= 0) {
     enemySet = false;
     $('.current-enemy').detach();
+    $('#attack-output').html(`<div class="col-4">You attacked ${enemyName} for 
+      ${currentAttack} damage.<br>${enemyName} attacked you back for ${enemyAttack} damage.</div>`);
   } else if (charhp <= 0) {
     $('#attack-output').html(`<div class="col-4">You've been defeated... GAME OVER!!!</div><div class="col-8"></div><div class="col-4"><button id="restart">Restart</button></div>`);
   }
+  
 }
 
 function handleRestartClick() {
